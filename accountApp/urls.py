c28d6977a75881db631562f4eb686ac809c2a83c
jonghyeok <alb7979s@naver.com>
2020-12-17 21:47:49 +0900

    header, footer style

---
+++ accountApp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from accountApp.views import hello_world
+
+app_name = 'accountApp'
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world'),
+]
