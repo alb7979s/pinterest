469ca94677d220dfac1f1bd12a7d98732a0d659e
jonghyeok <alb7979s@naver.com>
2020-12-18 09:59:36 +0900

    CSS file separate

--- config/settings.py
+++ config/settings.py
@@ -123,4 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/head.html/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR/ "static",
+]
