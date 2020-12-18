146a81ba0a53fbc53fedf42b015f6ca538469cf1
jonghyeok <alb7979s@naver.com>
2020-12-18 19:50:15 +0900

    POST 통신을 통한 DB관리

--- accountApp/views.py
+++ accountApp/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountApp.models import HelloWorld
+
+
 def hello_world(request):
-    return render(request, 'accountApp/hello_world.html')
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
+        # return HttpResponseRedirect(reverse('accountApp: hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
