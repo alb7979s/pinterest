34c57fcc7356491cce9b8e6529b525eda1271744
jonghyeok <alb7979s@naver.com>
2020-12-22 16:31:28 +0900

    CreateView를 통한 회원가입

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from accountApp.views import hello_world
+from accountApp.views import hello_world, AccountCreateView
 
 app_name = 'accountApp'
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
