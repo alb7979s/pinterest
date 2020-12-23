1d23667d249487e537a1517b986816e3f2fc3448
jonghyeok <alb7979s@naver.com>
2020-12-23 12:36:37 +0900

    decorator를 이용한 코드 간소화

--- accountApp/views.py
+++ accountApp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
@@ -5,27 +6,26 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountApp.decorators import account_ownership_required
 from accountApp.forms import AccountUpdateForm
 from accountApp.models import HelloWorld
 
-
+@login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            temp = request.POST.get('hello_world_input')
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
-            hello_world_list = HelloWorld.objects.all()
-            # return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
-            return HttpResponseRedirect(reverse('accountApp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        hello_world_list = HelloWorld.objects.all()
+        # return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountApp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountApp:login'))
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 class AccountCreateView(CreateView):
     model = User
@@ -38,6 +38,10 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountApp/detail.html'
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+@method_decorator(account_ownership_required, 'get')
+@method_decorator(account_ownership_required, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
@@ -55,6 +59,10 @@ class AccountUpdateView(UpdateView):
         else:
             return HttpResponseForbidden()
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+@method_decorator(account_ownership_required, 'get')
+@method_decorator(account_ownership_required, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
