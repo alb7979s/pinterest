4c43dd38c06b17aa8273be6e2b07398219744d43
jonghyeok <alb7979s@naver.com>
2020-12-24 10:55:07 +0900

    profileApp - CreateView & UpdateView

--- profileApp/urls.py
+++ profileApp/urls.py
@@ -1,5 +1,7 @@
-
+from profileApp.views import ProfileCreateView, ProfileUpdateView
+from django.urls import path
 app_name = 'profileApp'
 urlpatterns = [
-
+    path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
--- profileApp/views.py
+++ profileApp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+from profileApp.models import Profile
+from profileApp.forms import ProfileCreationForm
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountApp:hello_world')
+    template_name = 'profileApp/create.html'
 
-# Create your views here.
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        return super().form_valid(form)
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountApp:hello_world')
+    template_name = 'profileApp/update.html'
