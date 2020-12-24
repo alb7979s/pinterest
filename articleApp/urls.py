0b97db0da59c235c9412694733f64126ef8807fd
jonghyeok <alb7979s@naver.com>
2020-12-24 16:03:11 +0900

    listView - pagination 적용

--- articleApp/urls.py
+++ articleApp/urls.py
@@ -1,13 +1,13 @@
 from django.views.generic import TemplateView
 from django.urls import path
 
-from articleApp.views import ArticleUpdateView, ArticleCreateView, ArticleDetailView, ArticleDeleteView
+from articleApp.views import ArticleUpdateView, ArticleCreateView, ArticleDetailView, ArticleDeleteView, ArticleListView
 
 app_name = 'articleApp'
 urlpatterns =[
-    path('list/', TemplateView.as_view(template_name='articleApp/list.html'), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
+    path('list/', ArticleListView.as_view(), name='list'),
 ]
