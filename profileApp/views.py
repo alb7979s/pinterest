0fe4f9b47cb4c916b72958c6dc954706bcde9abf
jonghyeok <alb7979s@naver.com>
2020-12-24 10:59:34 +0900

    decorate

--- profileApp/views.py
+++ profileApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
+
+from profileApp.decorators import profile_ownership_required
 from profileApp.models import Profile
 from profileApp.forms import ProfileCreationForm
 class ProfileCreateView(CreateView):
@@ -16,6 +19,8 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
