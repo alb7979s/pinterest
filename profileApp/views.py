5e9768ae536a884e24d7f43143bc8e03d539aa48
jonghyeok <alb7979s@naver.com>
2020-12-24 11:56:16 +0900

    get_success_url & refactoring

--- profileApp/views.py
+++ profileApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -25,5 +25,7 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountApp:hello_world')
     template_name = 'profileApp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountApp:detail', kwargs={'pk': self.object.user.pk})
