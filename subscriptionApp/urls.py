b311a53aa29f150659c12238cbe34a816c76faeb
jonghyeok <alb7979s@naver.com>
2020-12-25 15:11:41 +0900

    field lookup을 이용한 subscription 페이지 구현

--- subscriptionApp/urls.py
+++ subscriptionApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from subscriptionApp.views import SubscriptionView
+from subscriptionApp.views import SubscriptionView, SubscriptionListView
 
 app_name = 'subscriptionApp'
 
 urlpatterns = [
     path('subscription/', SubscriptionView.as_view(), name='subscription'),
+    path('list/', SubscriptionListView.as_view(), name='list'),
 ]
--- subscriptionApp/views.py
+++ subscriptionApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleApp.models import Article
 from projectApp.models import Project
 from subscriptionApp.models import Subscription
 
@@ -19,3 +20,13 @@ class SubscriptionView(RedirectView):
         else:
             Subscription(user=user, project=project).save()
         return super(SubscriptionView, self).get(request, *args, **kwargs)
+
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscriptionApp/list.html'
+    paginate_by = 5
+    def get_queryset(self):
+        projects = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=projects)
+        return article_list
