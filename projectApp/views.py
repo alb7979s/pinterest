550727ba3f1beac7c1b6eb1b226f95fdd3965d47
jonghyeok <alb7979s@naver.com>
2020-12-29 12:57:59 +0900

    bug fix

--- projectApp/views.py
+++ projectApp/views.py
@@ -31,7 +31,8 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
         object_list = Article.objects.filter(project=self.get_object())
         if user.is_authenticated:
             subscription = Subscription.objects.filter(user=user, project=project)
-
+        else:
+            subscription = None
         return super(ProjectDetailView, self).get_context_data(object_list=object_list, subscription=subscription, **kwargs)
 
 class ProjectListView(ListView):
